9378910bfe038be4f4eff997e999cc4cd1c626ce
Zap <67962871+ziap@users.noreply.github.com>
2021-09-28 11:11:35 +0700

    Big website update

---
+++ script.js
@@ -0,0 +1,34 @@
+;(async () => {
+    document.getElementById('yo').innerHTML = new Date(Date.now() - new Date('1-12-2005')).getFullYear() - 1970
+    const response = await fetch('https://api.github.com/users/ziap/repos')
+    const data = await response.json()
+    data.filter(repo => repo.has_pages && repo.name != 'ziap.github.io').forEach(repo => {
+        const proj = document.createElement('a')
+        const container = document.createElement('div')
+        const img = new Image()
+        const title = document.createElement('span')
+        const titleContainer = document.createElement('div')
+        titleContainer.className = 'title-container'
+        proj.className = 'project'
+        proj.href = '/' + repo.name;
+        container.className = 'image-container'
+        img.className = 'thumbnail'
+        title.className = 'title'
+        img.src = `https://raw.githubusercontent.com/ziap/${repo.name}/${repo.default_branch}/logo.png`
+        img.alt = repo.name.split('-').join(' ')
+        title.innerHTML = img.alt.toUpperCase()
+        container.appendChild(img)
+        titleContainer.appendChild(title)
+        proj.appendChild(container)
+        proj.appendChild(titleContainer)
+        document.getElementById('projects').appendChild(proj)
+    })
+
+    document.getElementById('search-bar').addEventListener('keyup', e => {
+        for (const project of document.getElementsByClassName('project')) {
+            const title = project.getElementsByClassName('title')[0].innerHTML
+            if (e.target.value == '' || title.includes(e.target.value.toUpperCase())) project.style.display = ''
+            else project.style.display = 'none'
+        }
+    })
+})()
